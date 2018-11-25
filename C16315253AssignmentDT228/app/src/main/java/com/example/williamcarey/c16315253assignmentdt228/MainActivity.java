44b07b1191121454da458e75a11038217df837b9
Will <williamcarey1998@gmail.com>
2018-11-25 14:46:59 +0000

    added functionality to change the color of the buttons

--- C16315253AssignmentDT228/app/src/main/java/com/example/williamcarey/c16315253assignmentdt228/MainActivity.java
+++ C16315253AssignmentDT228/app/src/main/java/com/example/williamcarey/c16315253assignmentdt228/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.williamcarey.c16315253assignmentdt228;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-    @Override
+    Button register, reset;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (Button)findViewById(R.id.register);
+        register.setBackgroundColor(getResources().getColor(R.color.aqua));
+
+        reset =  (Button)findViewById(R.id.reset);
+        reset.setBackgroundColor(getResources().getColor(R.color.aqua));
     }
 }
